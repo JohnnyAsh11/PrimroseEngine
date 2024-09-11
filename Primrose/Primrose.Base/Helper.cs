488c440d4f5caa67b367dc25a54de2f6caeb0762
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2024-09-11 16:00:52 -0400

    The Helper class can now render debug info to the game window.

--- Primrose/Primrose.Base/Helper.cs
+++ Primrose/Primrose.Base/Helper.cs
@@ -17,8 +17,27 @@ namespace Primrose.Base
 
         // Fields:
         private static Dictionary<string, Texture2D> _gameTextures;
+        private static SpriteFont _font;
+        private static SpriteBatch _spriteBatch;
 
-        // Properties: - NONE -
+        // Properties:
+        /// <summary>
+        /// Get/Set property for the SpriteFont used to debug.
+        /// </summary>
+        public static SpriteFont Font
+        {
+            get { return _font; }
+            set { _font = value; }
+        }
+
+        /// <summary>
+        /// Get/Set property for the SpriteBatch used to debug draw.
+        /// </summary>
+        public static SpriteBatch SpriteBatch
+        {
+            get { return _spriteBatch; }
+            set { _spriteBatch = value; }
+        }
 
         // Methods:
         /// <summary>
@@ -36,6 +55,30 @@ namespace Primrose.Base
 
             // Setting the new rasterizer state to the GraphicsDevice.
             graphics.RasterizerState = rasterizerState;
+        }
+
+        /// <summary>
+        /// Renders a message to the game window
+        /// </summary>
+        /// <param name="message">Message being written to the screen.</param>
+        /// <param name="location">The location that the message is being rendered.</param>
+        /// <param name="spriteBatch">SpriteBatch reference to render the message.</param>
+        public static void DrawString(string message, Vector2 location, Color tint)
+        {
+            if (_spriteBatch is null)
+            {
+                throw new Exception("Helper's SpriteBatch reference is null.");
+            }
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.DrawString(
+                _font,
+                message,
+                location,
+                tint);
+
+            _spriteBatch.End();
         }
 
     }
