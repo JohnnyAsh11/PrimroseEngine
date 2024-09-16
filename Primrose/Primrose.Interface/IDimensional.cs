057e952dff10208f90f16f841831b0b5205000f6
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2024-09-16 16:56:52 -0400

    Added the IDimensional interface for objects with dimensions.

---
+++ Primrose/Primrose.Interface/IDimensional.cs
@@ -0,0 +1,23 @@
+﻿namespace Primrose.Interface
+{
+    /// <summary>
+    /// Contains properties for the dimensions of a 3D object.
+    /// </summary>
+    public interface IDimensional
+    {
+        /// <summary>
+        /// Tracks the width of an object with dimensions.
+        /// </summary>
+        public float Width { get; set; }
+
+        /// <summary>
+        /// Tracks the length of an object with dimensions.
+        /// </summary>
+        public float Length { get; set; }
+
+        /// <summary>
+        /// Tracks the length of an object with dimensions.
+        /// </summary>
+        public float Height { get; set; }
+    }
+}
